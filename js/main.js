edf921a03a3017bc6088be3aac0404d0fea06b14
UglanovEA <uglanovea@gmail.com>
2020-05-27 16:40:07 +0300

    ver 1.5

--- js/main.js
+++ js/main.js
@@ -1,35 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".about__button").click(function (event) {
+    event.preventDefault();
+    console.log("Click!");
 
-	$('.about__toggle').click(function(event){
-		event.preventDefault();
-		console.log('Click!');
+    $(".about-content-box").hide();
+    var href = $(this).attr("href"); // '#private'
 
-		$('.about-content__box').hide();
-		var href = $(this).attr('href'); // '#private'
+    console.log(href);
+    $(href).fadeIn();
+  });
 
-		console.log(href);
-		$(href).fadeIn();
-	});
+  var windowHeight = $(window).height();
+  console.log(windowHeight);
 
+  // Следим за скроллом, и показываем / скрываем кнопку
+  $(window).scroll(function () {
+    console.log($(this).scrollTop());
+    if ($(this).scrollTop() > windowHeight) {
+      $("#scrollToTop").fadeIn();
+    } else {
+      $("#scrollToTop").fadeOut();
+    }
+  });
 
-	var windowHeight = $(window).height();
-	console.log(windowHeight);
-
-	// Следим за скроллом, и показываем / скрываем кнопку
-	$(window).scroll(function(){
-		console.log( $(this).scrollTop() );
-		if ( $(this).scrollTop() > windowHeight) {
-			$('#scrollToTop').fadeIn();
-		} else {
-			$('#scrollToTop').fadeOut();
-		}
-	});
-
-	// При клике на кнопку - делаем прокуртку наверх страницы
-	$('#scrollToTop').click(function(event){
-		console.log(11111);
-		event.preventDefault();
-		$('html').animate({scrollTop: 0}, 800)
-	});
-
+  // При клике на кнопку - делаем прокуртку наверх страницы
+  $("#scrollToTop").click(function (event) {
+    console.log(11111);
+    event.preventDefault();
+    $("html").animate({ scrollTop: 0 }, 800);
+  });
 });
